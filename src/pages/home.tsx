e41cc59b48cf3e92288511fb27cb07d649432130
yuval-benjamin <jjuunnkkmm@gmail.com>
2025-01-07 01:19:35 +0200

    making home prettier

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -29,12 +29,27 @@ const Home: React.FC = () => {
   }, []);
 
   return (
-    <div className="d-flex flex-column align-items-center bg-light vh-100 p-4">
+    <div>
       <Header />
-      <div className="card-container">
-        <img src="/full_logo.png" alt="Logo" className="full-logo" />
-        <CreatePostPage />
-        <Posts posts={posts} />
+      <div className="container mt-5 d-flex justify-content-center">
+        <div className="card p-3 w-100 text-center">
+          <div className="card-body">
+            <img src="/full_logo.png" alt="Logo" className="img-fluid mb-3" style={{ height: '200px' }} />
+            <div className="w-100">
+              <CreatePostPage />
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div className="container mt-5">
+        <div className="card p-3">
+          <div className="card-body">
+            <div className="w-100">
+              <Posts posts={posts} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
